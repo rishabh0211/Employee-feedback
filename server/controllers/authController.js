4674eb4c0ca643daa5ba395fabcb0306e6d0ba98
rishabh0211 <rishabh0211@gmail.com>
2020-09-11 16:58:13 +0530

    sign in and sign up apis

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,45 @@
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const passport = require("passport");
+const { body, sanitizeBody, check, validationResult } = require("express-validator");
+
+exports.validateSignupRules = () => {
+  return [
+    check("email", "Enter a valid email").isEmail().normalizeEmail(),
+    check("password", "Enter a Password").notEmpty(),
+    check("password", "Password must be atleast 6 characters long").isLength({ min: 6 })
+  ];
+};
 
 exports.validateSignup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const firstError = errors.errors.map(error => error.msg)[0];
+  return res.status(400).send({ messgage: firstError });
+};
 
+exports.signup = async (req, res, next) => {
+  const { name, email, password, admin = false } = req.body;
+  const user = await new User({ name, email, password, admin });
+  await user.save();
+  res.status(201).send(user);
+};
+
+exports.signin = async (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    if (!user) {
+      return res.status(400).json({ message: info.message });
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+      res.json(user);
+    });
+  })(req, res, next);
 };
