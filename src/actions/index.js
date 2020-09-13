079d22ca9221ce0c553b45e623b2072b6e3000f5
rishabh0211 <rishabh0211@gmail.com>
2020-09-13 23:44:56 +0530

    api integrated in store

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,11 @@ export const loginUser = (email, password) => {
     dispatch(getActionObj(actionTypes.USER_LOGIN_START));
     return fetch(`${actionTypes.API_ENDPOINT}/api/auth/signin`, {
       method: "POST",
+      mode: "cors",
       headers: {
         'Content-Type': 'application/json'
       },
+      credentials: "include",
       body: JSON.stringify({ email, password })
     })
       .then(res => res.json())
@@ -19,4 +21,49 @@ export const loginUser = (email, password) => {
         console.log(e);
       });
   };
+};
+
+export const fetchAllUsers = () => {
+  return dispatch => {
+    dispatch(getActionObj(actionTypes.GET_USERS_START));
+    return fetch(`${actionTypes.API_ENDPOINT}/api/users/getAll`, {
+      credentials: "include",
+      mode: "cors"
+    })
+      .then(res => res.json())
+      .then(data => {
+        dispatch(getActionObj(actionTypes.GET_USERS_SUCCESS, { users: data.users }));
+      });
+  };
+};
+
+export const fetchUserProfile = userId => {
+  return dispatch => {
+    dispatch(getActionObj(actionTypes.GET_USER_PROFILE_START));
+    return fetch(`${actionTypes.API_ENDPOINT}/api/users/${userId}`, {
+      credentials: "include"
+    })
+      .then(res => res.json())
+      .then(user => {
+        dispatch(getActionObj(actionTypes.GET_USER_PROFILE_SUCCESS, { user }));
+      });
+  };
+};
+
+export const editFeedback = (feedbackId, message) => {
+  return dispatch => {
+    dispatch(getActionObj(actionTypes.EDIT_FEEDBACK_START));
+    return fetch(`${actionTypes.API_ENDPOINT}/api/feedback/${feedbackId}`, {
+      method: "POST",
+      body: JSON.stringify({ message }),
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(user => {
+        dispatch(getActionObj(actionTypes.EDIT_FEEDBACK_SUCCESS, { feedbackId, message }));
+      });
+  }
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,8 @@ const getInitalState = () => (
   {
     users: [],
     currentUser: null,
-    isLoading: false
+    isLoading: false,
+    userDetails: null,
   }
 );
 
@@ -20,6 +21,45 @@ const userReducer = (state = getInitalState(), { type, payload }) => {
         ...state,
         isLoading: false,
         currentUser: payload.user
+      };
+    case actionsTypes.GET_USERS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionsTypes.GET_USERS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        users: payload.users
+      };
+    case actionsTypes.GET_USER_PROFILE_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionsTypes.GET_USER_PROFILE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        userDetails: payload.user
+      };
+    case actionsTypes.EDIT_FEEDBACK_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionsTypes.EDIT_FEEDBACK_SUCCESS:
+      const userDetails = JSON.parse(JSON.stringify(state.userDetails));
+      userDetails.feedbacks.forEach(feedback => {
+        if (feedback._id === payload.feedbackId) {
+          feedback.feedback = payload.message;
+        }
+      });
+      return {
+        ...state,
+        isLoading: false,
+        userDetails
       };
     default:
       return state;
