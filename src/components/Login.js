4671fb54315d6971eba5498d03dd78f52930c0e3
rishabh0211 <rishabh0211@gmail.com>
2020-09-14 03:25:38 +0530

    removed the set state

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,8 @@ import { useHistory } from "react-router-dom";
 
 const Login = ({ user, loginUser }) => {
   const history = useHistory();
-  const [email, setEmail] = useState('messi@gmail.com');
-  const [password, setPassword] = useState('pass1234');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   useEffect(() => {
